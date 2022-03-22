a48caea3941ffe24f601f141af880a6d5692c0eb
Kenneth J. Yang <kjy5@uw.edu>
2022-03-22 10:28:27 -0700

    Add legacy support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/honors-portfolio/',
+    plugins: [
+        react(),
+        legacy({
+            targets: ['defaults', 'ie >= 11'],
+            additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+        }),
+    ],
+    base: '/honors-portfolio/',
 })
