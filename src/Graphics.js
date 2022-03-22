4376b6b16d4c4827fc6d6ae241ca893ff8a8c557
Kenneth J. Yang <kjy5@uw.edu>
2022-03-22 14:22:18 -0700

    3D text with custom font

--- src/Graphics.js
+++ src/Graphics.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import {FontLoader} from "three/examples/jsm/loaders/FontLoader";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import {TextGeometry} from "three/examples/jsm/geometries/TextGeometry";
 import WebGL from "three/examples/jsm/capabilities/WebGL.js";
 
@@ -14,29 +15,47 @@ export default function Graphics() {
     renderer.setSize(window.innerWidth, window.innerHeight)
     camera.position.z = 5
 
+    // Add OrbitControls
+    const controls = new OrbitControls(camera, renderer.domElement)
+
+    // Add a light
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+    directionalLight.position.set(3, 3, 10)
+    scene.add(ambientLight)
+    scene.add(directionalLight)
+
     // Add Title
     const fontLoader = new FontLoader()
-    fontLoader.load('node_modules/three/examples/fonts/helvetiker_regular.typeface.json', function (font) {
-        const textGeometry = new TextGeometry('Hello World', {
+    fontLoader.load('src/assets/fonts/Coolvetica Rg_Regular.json', function (font) {
+        const textGeometry = new TextGeometry("Kenneth's Honors Portfolio", {
             font: font,
-            size: 1,
-            height: 0.1,
-            curveSegments: 12,
-            bevelEnabled: true,
-            bevelThickness: 0.1,
-            bevelSize: 0.1,
-            bevelSegments: 5
+            size: .75,
+            height: 0.075,
+            curveSegments: 8,
+            bevelEnabled: false,
+            bevelThickness: 0.05,
+            bevelSize: 0.005,
+            bevelSegments: 1
         })
-        const textMaterial = new THREE.MeshBasicMaterial({color: 0xffffff})
+        const textMaterial = new THREE.MeshPhongMaterial({color: 0xffffff})
         const textMesh = new THREE.Mesh(textGeometry, textMaterial)
-        textMesh.position.x = -2
-        textMesh.position.y = 2
+        textMesh.geometry.computeBoundingBox()
+
+        textMesh.position.x = -textMesh.geometry.boundingBox.max.x / 2
+
+        const boxHelper = new THREE.BoxHelper(textMesh, 0xffff00)
+        const gridHelper = new THREE.GridHelper(10, 10)
         scene.add(textMesh)
+        scene.add(boxHelper)
+        scene.add(gridHelper)
     })
 
     // Render
     function animate() {
         requestAnimationFrame(animate);
+
+        controls.update()
 
         renderer.render(scene, camera);
     }
