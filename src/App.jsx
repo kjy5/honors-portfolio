46cca2009c095aeffddc20fa4de04df0bdc1e86f
Kenneth J. Yang <kjy5@uw.edu>
2022-03-22 00:05:19 -0700

    DeepSource fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import React from 'react'
 import './styles/App.css'
 
+/**
+ * Website base component; where all other components are composed.
+ * @returns {JSX.Element}
+ */
 function App() {
 
     return (
         <div className="App">
-            <h1>Kenneth's Honors Portfolio</h1>
+            <h1>Kenneth&apos;s Honors Portfolio</h1>
             <p>This is my portfolio website! It is currently under active development.</p>
             <p>Please see my <a href="https://kjy509.wixsite.com/honors-portfolio">temporary website</a> while I work
                 to complete this website!</p>
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/index.css'
 import App from './App'
 import Graphics from './Graphics'
+import './styles/index.css'
 
 // Run Graphics
 Graphics()
