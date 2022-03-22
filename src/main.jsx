13f5e6df7a33cb510b65d18a331ad6d1741af56a
Kenneth J. Yang <kjy5@uw.edu>
2022-03-21 18:36:37 -0700

    Three and react proof of concept

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import Graphics from './Graphics'
+
+Graphics()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 )
