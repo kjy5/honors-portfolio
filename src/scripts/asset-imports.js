b108d8c4f7135fcf9c1891bed4857c44a6a08ddb
Kenneth J. Yang <kjy5@uw.edu>
2022-03-25 12:10:03 -0700

    Fixed meta import urls

--- src/scripts/asset-imports.js
+++ src/scripts/asset-imports.js
@@ -6,7 +6,7 @@ import starting_new_honors_portfolio_website_0
 import starting_new_honors_portfolio_website_1
     from '../assets/content/0/1/Starting New Honors Portfolio Website/1.webp';
 import starting_new_honors_portfolio_website_meta
-    from '../assets/content/0/1/Starting New Honors Portfolio Website/meta.tsv';
+    from '../assets/content/0/1/Starting New Honors Portfolio Website/meta.tsv?url';
 
 // Leadership and Democracy Final Paper
 import leadership_democracy_final_paper_0 from '../assets/content/0/1/Leadership and Democracy Final Paper/0.pdf';
@@ -18,11 +18,11 @@ import nautilus_render_2 from '../assets/content/0/0/Nautilus Renders/2.webp';
 import nautilus_render_3 from '../assets/content/0/0/Nautilus Renders/3.webp';
 import nautilus_render_4 from '../assets/content/0/0/Nautilus Renders/4.webp';
 import nautilus_render_5 from '../assets/content/0/0/Nautilus Renders/5.webp';
-import nautilus_render_meta from '../assets/content/0/0/Nautilus Renders/meta.tsv';
+import nautilus_render_meta from '../assets/content/0/0/Nautilus Renders/meta.tsv?url';
 
 // COSMOS Interface
 import cosmos_interface_0 from '../assets/content/0/0/COSMOS Interface/0.webp';
-import cosmos_interface_meta from '../assets/content/0/0/COSMOS Interface/meta.tsv';
+import cosmos_interface_meta from '../assets/content/0/0/COSMOS Interface/meta.tsv?url';
 
 // Fall Quarter Reflection
 import fall_quarter_reflection_0 from '../assets/content/0/0/Fall Quarter Reflection/0.pdf';
