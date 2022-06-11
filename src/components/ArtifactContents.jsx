8d730a4b7222a4e899969cd38490affa6c45f318
Kenneth J. Yang <kjy5@uw.edu>
2022-06-11 11:34:59 -0700

    Fixed iframe, switched to createRoot

--- src/components/ArtifactContents.jsx
+++ src/components/ArtifactContents.jsx
@@ -1,17 +1,17 @@
-import "../styles/ArtifactContents.css";
-import { embedAssets } from "../scripts/asset-imports";
-import Gallery from "./Gallery";
-import PropTypes from "prop-types";
-import React from "react";
+import '../styles/ArtifactContents.css'
+import { embedAssets } from '../scripts/asset-imports'
+import Gallery from './Gallery'
+import PropTypes from 'prop-types'
+import React from 'react'
 
 /**
  * Contents of an artifact including images, embedded items, and text
  * @param {object} props
  * @returns {JSX.Element} Artifact contents component
  */
-export default function ArtifactContents(props) {
+export default function ArtifactContents (props) {
   // Destructure props
-  const { title, hasEmbed, hasImages, text } = props;
+  const { title, hasEmbed, hasImages, text } = props
 
   return (
     <div className="ArtifactContents" id={title}>
@@ -36,7 +36,6 @@ export default function ArtifactContents(props) {
                 key={embedSrc}
                 className="ArtifactContents__iframe"
                 src={embedSrc}
-                frameBorder="0"
                 title={title}
               />,
             ];
