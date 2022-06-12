e79ec18956fa006c1b0e1560ad447b633398aac5
Kenneth J. Yang <kjy5@uw.edu>
2022-06-12 11:21:43 -0700

    Installed react router and added basic setup

--- src/components/Artifacts.jsx
+++ src/components/Artifacts.jsx
@@ -1,54 +1,56 @@
-import "../styles/Artifacts.css";
+import '../styles/Artifacts.css'
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 
-import ArtifactCard from "./ArtifactCard";
-import ArtifactContents from "./ArtifactContents";
-import content from "../assets/content.tsv?url";
+import ArtifactCard from './ArtifactCard'
+import ArtifactContents from './ArtifactContents'
+import content from '../assets/content.tsv?url'
+import Graphics from '../scripts/graphics'
 // noinspection ES6CheckImport
-import { tsv } from "d3";
+import { tsv } from 'd3'
 
 /**
  * @description Parent component holding Artifact Card and Artifact Contents
  * @returns {JSX.Element[]} Array of Artifacts
  * @constructor
  */
-export default function Artifacts() {
+export default function Artifacts () {
   // Update against new content data
-  const [contentData, setContentData] = useState([]);
+  const [contentData, setContentData] = useState([])
 
   // Get content data
   useEffect(() => {
-    tsv(content).then((data) => setContentData(data));
-  }, []);
+    Graphics()
+    tsv(content).then((data) => setContentData(data))
+  }, [])
 
   // Render Artifacts
   // Hold artifacts as they are created
-  const output = [];
+  const output = []
 
   // Year and quarter tracker
-  let curYear = -1;
-  let curQuarter = -1;
+  let curYear = -1
+  let curQuarter = -1
 
   // Iterate through content data
   contentData.forEach((artifact) => {
     // Insert year header if necessary
     if (artifact.year !== curYear) {
-      curYear = artifact.year;
+      curYear = artifact.year
       output.push(
-        <div className="Artifacts__year-header" key={`year_${curYear}`} />
-      );
+        <div className="Artifacts__year-header" key={`year_${curYear}`}/>
+      )
     }
 
     // Insert quarter header if necessary
     if (artifact.quarter !== curQuarter) {
-      curQuarter = artifact.quarter;
+      curQuarter = artifact.quarter
       output.push(
         <div
           className="Artifacts__quarter-header"
           key={`year_${curYear}_quarter_${curQuarter}`}
         />
-      );
+      )
     }
 
     // Insert artifact card, contents, and divider
@@ -68,10 +70,10 @@ export default function Artifacts() {
         hasImages={artifact.hasImages}
         hasEmbed={artifact.hasEmbed}
       />,
-      <div key={artifact.subtitle} className="Artifacts__spacers" />
-    );
-  });
+      <div key={artifact.subtitle} className="Artifacts__spacers"/>
+    )
+  })
 
   // Return artifacts
-  return output;
+  return output
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,25 @@
-import "./styles/index.css";
+import './styles/index.css'
 
-import Artifacts from "./components/Artifacts";
-import Graphics from "./scripts/graphics";
-import InProgress from "./components/InProgress";
-import React from "react";
-import { createRoot } from "react-dom/client";
+import Artifacts from './components/Artifacts'
+import InProgress from './components/InProgress'
+import React from 'react'
+// noinspection ES6CheckImport
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
 
-// Run Graphics
-Graphics();
-
-// Render HTML
-const root = createRoot(document.querySelector("main"));
+const root = createRoot(document.querySelector('main'))
 root.render(
-  <React.StrictMode>
-    <div id="top" />
-    <InProgress />
-    <Artifacts />
-  </React.StrictMode>
-);
+  <BrowserRouter>
+    <Routes>
+      <Route path="/honors-portfolio" element={
+        <React.StrictMode>
+          <div id="top"/>
+          <InProgress/>
+          <Artifacts/>
+        </React.StrictMode>
+      }/>
+      <Route path="/honors-portfolio/hi" element={<div>Hi</div>}/>
+      <Route path="*" element={<div>404</div>}/>
+    </Routes>
+  </BrowserRouter>
+)
