1c0915a4897cdcaf3900cf2813dd976cad4e5580
Kenneth J. Yang <kjy5@uw.edu>
2022-06-13 00:18:57 -0700

    Started implementing, having trouble with more items

--- src/components/ArtifactContents.jsx
+++ src/components/ArtifactContents.jsx
@@ -1,62 +1,93 @@
-import "../styles/ArtifactContents.css";
-import { embedAssets, linkAssets } from "../scripts/asset-imports";
-import Gallery from "./Gallery";
-import React from "react";
+import '../styles/ArtifactContents.css'
+import { embedAssets, linkAssets } from '../scripts/asset-imports'
+import { getURLName } from '../scripts/content-handler'
+import Gallery from './Gallery'
+import React from 'react'
 
 /**
  * Contents of an artifact including images, embedded items, and text
  * @param {object} props content data for a single artifact
  * @returns {JSX.Element} Artifact contents component
  */
-export default function ArtifactContents(props) {
+export default function ArtifactContents (props) {
   // Destructure props
-  const { artifact } = props;
+  const { artifact } = props
   const { title, subtitle, hasEmbed, hasImages, hasLink, text, year, quarter } =
-    artifact;
+    artifact
+
+  const quarterToString = () => {
+    switch (quarter) {
+      case '0':
+        return 'Fall'
+      case '1':
+        return 'Winter'
+      case '2':
+        return 'Spring'
+      case '3':
+        return 'Summer'
+      default:
+        return ''
+    }
+  }
+
+  const yearToString = () => {
+    switch (year) {
+      case '0':
+        return 'Freshman'
+      case '1':
+        return 'Sophomore'
+      case '2':
+        return 'Junior'
+      case '3':
+        return 'Senior'
+      default:
+        return ''
+    }
+  }
 
   // Blur canvas background
-  document.querySelector("#canvas").classList.add("blur");
+  document.querySelector('#canvas').classList.add('blur')
 
   // Back button functionality
-  const backButtonCallback = React.useCallback(() => history.back(), []);
+  const backButtonCallback = React.useCallback(() => history.back(), [])
 
   return (
     <div className="ArtifactContents" id={title}>
       {/* Back button */}
-      <button onClick={backButtonCallback} type="button">
+      <button className="ArtifactCard__return" onClick={backButtonCallback} type="button">
         Return to Kenneth&apos;s Honors Portfolio
       </button>
 
+      {/* 3D graphics element */}
+      <div className="ArtifactCard__graphics" id={getURLName(title)}/>
+
       {/* Title and subtitle */}
-      <div className="ArtifactContents__title">{title}</div>
-      <div className="ArtifactContents__subtitle">{subtitle}</div>
+      <div className="ArtifactContents__title"><h1>{title}</h1></div>
+      <div className="ArtifactContents__subtitle"><h2>{subtitle}</h2></div>
 
       {/* Year and quarter */}
       <div className="ArtifactContents__date">
-        {quarter} {year}
+        {yearToString()} {quarterToString()}
       </div>
 
       {/* Images, use Gallery if there are images */}
       <div className="ArtifactContents__images">
-        {hasImages !== "" && <Gallery title={title} />}
+        {hasImages !== '' && <Gallery title={title}/>}
       </div>
 
       {/* External Link */}
-      <div className="ArtifactContents__external-link">
-        {hasLink !== "" &&
+      <div className="ArtifactContents__external-links">
+        {hasLink !== '' &&
           linkAssets[title].map((linkSrc) => {
-            return [
-              <a key={linkSrc} href={linkSrc}>
-                {linkSrc}
-              </a>,
-              <br key={`br_${linkSrc}`} />,
-            ];
+            return <a key={linkSrc} href={linkSrc}>
+              {linkSrc}
+            </a>
           })}
       </div>
 
       {/* Embedded items */}
       <div className="ArtifactContents__embeds">
-        {hasEmbed !== "" &&
+        {hasEmbed !== '' &&
           embedAssets[title].map((embedSrc) => {
             // Provide fallback external link and iframe
             return [
@@ -73,12 +104,12 @@ export default function ArtifactContents(props) {
                 src={embedSrc}
                 title={title}
               />,
-            ];
+            ]
           })}
       </div>
 
       {/* Text */}
       <p className="ArtifactContents__text">{text}</p>
     </div>
-  );
+  )
 }
