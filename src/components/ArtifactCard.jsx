3553aa86941eadb024056abc44056e36daaebbae
Kenneth J. Yang <kjy5@uw.edu>
2022-07-10 10:25:17 -0700

    Changed highlighting zone

--- src/components/ArtifactCard.jsx
+++ src/components/ArtifactCard.jsx
@@ -1,21 +1,21 @@
-import "../styles/ArtifactCard.css";
+import '../styles/ArtifactCard.css'
 
-import { getURLName } from "../scripts/content-handler";
-import { Link } from "react-router-dom"; // skipcq: JS-0249
-import React from "react";
+import { getURLName } from '../scripts/content-handler'
+import { Link } from 'react-router-dom' // skipcq: JS-0249
+import React from 'react'
 
 /**
  * @description Overview card for an Artifact
  * @param {object} props props passed in from parent component
  * @returns {JSX.Element} The card
  */
-export default function ArtifactCard(props) {
+export default function ArtifactCard (props) {
   // Destructure props
-  const { artifact } = props;
-  const { title, subtitle } = artifact;
+  const { artifact } = props
+  const { title, subtitle } = artifact
 
   // Generate URL from title
-  const urlName = getURLName(title);
+  const urlName = getURLName(title)
 
   return (
     <Link
@@ -24,17 +24,19 @@ export default function ArtifactCard(props) {
       tabIndex="0"
     >
       {/* 3D graphics element */}
-      <div className="ArtifactCard__graphics" id={urlName} />
+      <div className="ArtifactCard__graphics" id={urlName}/>
 
-      {/* Title */}
-      <div className="ArtifactCard__title">
-        <h1>{title}</h1>
-      </div>
+      <div className="ArtifactCard__text">
+        {/* Title */}
+        <div className="ArtifactCard__title">
+          <h1>{title}</h1>
+        </div>
 
-      {/* Subtitle */}
-      <div className="ArtifactCard__subtitle">
-        <h3>{subtitle}</h3>
+        {/* Subtitle */}
+        <div className="ArtifactCard__subtitle">
+          <h3>{subtitle}</h3>
+        </div>
       </div>
     </Link>
-  );
+  )
 }
