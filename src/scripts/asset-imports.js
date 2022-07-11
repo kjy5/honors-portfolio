3cd167715a71bb8556f7a0ae8456c2b2e26c3dfb
Kenneth J. Yang <kjy5@uw.edu>
2022-07-11 00:00:50 -0700

    Leadership and democracy final paper

--- src/scripts/asset-imports.js
+++ src/scripts/asset-imports.js
@@ -12,6 +12,7 @@ import multimodalCompositionFinalPortfolioLinkMeta
   from '../assets/link_metas/Multimodal Composition Final Portfolio.tsv?url'
 import nasaSummerUndergraduateResearchProgramLinkMeta
   from '../assets/link_metas/NASA Summer Undergraduate Research Program.tsv?url'
+import scene from '../assets/scene.json?url'
 
 // ============================
 // Package and export
@@ -44,15 +45,13 @@ export const embedAssets = {
   ],
 }
 
-// import scene from '../assets/scene.json?url'
-
 export const graphicAssets = {
   'International Brain Lab Position': 'https://res.cloudinary.com/kjy5/raw/upload/v1657473196/Honors%20Portfolio/International%20Brain%20Lab%20Position/Brain_tozt9h.json',
   'Multimodal Composition Final Portfolio': 'https://res.cloudinary.com/kjy5/raw/upload/v1657424232/Honors%20Portfolio/Multimodal%20Composition%20Final%20Portfolio/intro_image_wfaf5l.json',
   'NASA Summer Undergraduate Research Program': 'https://res.cloudinary.com/kjy5/raw/upload/v1657510709/Honors%20Portfolio/NASA%20Summer%20Undergraduate%20Research%20Program/hsl_q0tmeu.json',
   'Starting a New Honors Portfolio Website': 'https://res.cloudinary.com/kjy5/raw/upload/v1657518825/Honors%20Portfolio/Starting%20a%20New%20Honors%20Portfolio%20Website/website_bgeb18.json',
-  'ROV22 Simulation Demo': '',
-  'Leadership and Democracy Final Paper': '',
+  'ROV22 Simulation Demo': scene,
+  'Leadership and Democracy Final Paper': 'https://res.cloudinary.com/kjy5/raw/upload/v1657522828/Honors%20Portfolio/Leadership%20and%20Democracy%20Final%20Paper/door_c772st.json',
   'Nautilus Renders': 'https://res.cloudinary.com/kjy5/raw/upload/v1657424372/Honors%20Portfolio/Nautilus%20Renders/Nautilus_mj4ikr.json',
   'Evil Hangman': '',
   'COSMOS Interface': '',
