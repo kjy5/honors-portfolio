09daaf4242012ae6557b3c71c887082779110173
Kenneth Yang <kjy5@uw.edu>
2022-12-19 10:05:10 -0800

    Added coming soon component

---
+++ src/components/ComingSoon.tsx
@@ -0,0 +1,13 @@
+// <reference path="react.d.ts" />
+import {Fragment} from "react";
+
+export default function ComingSoon(): JSX.Element {
+    return (
+        <Fragment>
+            <h1>👋 Hello!</h1>
+            <h3>Welcome and thanks for visiting my honors portfolio website!</h3>
+            <p>The website is currently being updated. Feel free to scroll around and see the current progress, or come
+                back later when it's all done!</p>
+        </Fragment>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
+import ComingSoon from './components/ComingSoon'
 import './styles/index.sass'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
+        <ComingSoon/>
         <App/>
     </React.StrictMode>,
 )
