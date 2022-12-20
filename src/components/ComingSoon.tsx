632f923eabdeba05d12f815a1c5b022f20c74d3e
Kenneth Yang <kjy5@uw.edu>
2022-12-19 23:52:15 -0800

    DeepSource fixes

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -1,12 +1,17 @@
 import "../styles/ComingSoon.sass";
 
+/**
+ * Creates a React component that displays a "Coming Soon" message.
+ * @constructor
+ * @returns {JSX.Element}
+ */
 export default function ComingSoon(): JSX.Element {
     return (
         <div className={"coming-soon"}>
             <h1>👋 Hello!</h1>
             <h2>Welcome and thanks for visiting my honors portfolio website!</h2>
             <p>The website is currently being updated. Feel free to scroll around and see the current progress, or come
-                back later when it's all done!</p>
+                back later when it&apos;s all done!</p>
         </div>
     );
 }
