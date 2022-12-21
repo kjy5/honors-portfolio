885461895ef95caa8a05f70c21c527c4afbc8b9f
Kenneth Yang <kjy5@uw.edu>
2022-12-20 19:33:33 -0800

    Merged TSV files

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import ComingSoon from './components/ComingSoon'
 import './styles/index.sass'
 import Term from "./components/Term";
+import contentString from "./assets/content.tsv?raw";
+
+// Parse content data
+const content = contentString.split("\n").map((row) => {
+    return row.split("\t");
+});
+console.debug(content);
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
