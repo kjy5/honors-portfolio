825ac4e5c83925ce0b8c0370faf1c06640ad306d
Kenneth Yang <kjy5@uw.edu>
2022-12-22 01:27:17 -0800

    Reformat with JavaScript standard

--- src/main.tsx
+++ src/main.tsx
@@ -2,27 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ComingSoon from './components/ComingSoon'
 import './styles/index.sass'
-import ParseData from "./scripts/parse-data";
-import {Artifact} from "./scripts/interfaces";
-import Year from "./components/Year";
+import ParseData from './scripts/parse-data'
+import { Artifact } from './scripts/interfaces'
+import Year from './components/Year'
 
 // Parse data and return as an array of Artifact objects
-const artifacts: Artifact[] = ParseData();
-
+const artifacts: Artifact[] = ParseData()
 
 // Compute year set (get unique years)
-const years: Set<number> = new Set();
+const years: Set<number> = new Set()
 artifacts.forEach((artifact: Artifact) => {
-    years.add(artifact.year);
-});
+  years.add(artifact.year)
+})
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <ComingSoon/>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <ComingSoon/>
 
-        {/* Artifacts */}
-        {Array.from(years).map((year: number) =>
-            <Year key={year} filteredArtifacts={artifacts.filter((artifact: Artifact) => artifact.year === year)}/>
-        )}
-    </React.StrictMode>
-);
+    {/* Artifacts */}
+    {Array.from(years).map((year: number) =>
+      <Year key={year} filteredArtifacts={artifacts.filter(
+        (artifact: Artifact) => artifact.year === year)}/>,
+    )}
+  </React.StrictMode>,
+)
