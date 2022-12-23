b04698564431e166a3d3839574b27eea96e9206c
kjy5 <kjy5@users.noreply.github.com>
2022-12-23 07:06:36 +0000

    Reformat and Lint

--- src/components/Quarter.tsx
+++ src/components/Quarter.tsx
@@ -1,6 +1,6 @@
-import '../styles/Quarter.sass'
-import { Artifact } from '../scripts/interfaces'
-import ArtifactCard from './ArtifactCard'
+import "../styles/Quarter.sass";
+import { Artifact } from "../scripts/interfaces";
+import ArtifactCard from "./ArtifactCard";
 
 /**
  * Quarter component. Groups a quarter heading with a grid of artifacts.
