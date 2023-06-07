c895838c53b3cd0a500b9b177b79fc91d565700a
Kenneth Yang <kjy5@uw.edu>
2023-06-07 15:00:08 -0700

    Updated eslint to match new Vite standard

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,24 @@
 module.exports = {
+  env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react-hooks/recommended",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ["./tsconfig.json"],
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", 'react-refresh'],
   root: true,
   ignorePatterns: ["dist", "vite.config.ts", "src/components/Gallery.jsx"],
   rules: {
     "sort-imports": "error",
+    'react-refresh/only-export-components': 'warn',
   },
 };
