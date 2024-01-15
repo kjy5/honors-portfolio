27a09710ec6d1408f3f48d0c0b66d0d4165ab797
Kenneth Yang <kjy5@uw.edu>
2024-01-15 13:27:28 -0800

    Setup storybook, create themes

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,12 @@
-import type {Preview} from '@storybook/react';
+import type { Preview } from '@storybook/react';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { darkTheme, lightTheme } from '../src/themes';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 const preview: Preview = {
   parameters: {
@@ -12,4 +20,28 @@ const preview: Preview = {
   },
 };
 
+const decorators = [
+  withThemeFromJSXProvider({
+    themes: {
+      light: lightTheme,
+      dark: darkTheme,
+    },
+    defaultTheme: 'dark',
+    Provider: ThemeProvider,
+    GlobalStyles: CssBaseline,
+  }),
+];
+
+const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    expanded: true, // Adds the description and default columns
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export { decorators, parameters };
 export default preview;
