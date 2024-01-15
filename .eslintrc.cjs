38306a9708b422aa6d21726b6c2b4866beabb842
Kenneth Yang <kjy5@uw.edu>
2024-01-15 12:58:06 -0800

    Updated workflows and dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,29 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:jsx-a11y/recommended',
+    'eslint-config-prettier',
   ],
+  settings: {
+    react: { version: 'detect' },
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
-    ],
+      { allowConstantExport: true }],
   },
-}
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+};
