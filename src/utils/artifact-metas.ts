90e77dcc905e524b1cc24b6c5439d39ce78fe30d
Kenneth Yang <kjy5@uw.edu>
2024-01-16 23:52:50 -0800

    Extract artifacts

---
+++ src/utils/artifact-metas.ts
@@ -0,0 +1,6 @@
+import {ArtifactMetas} from "../models/artifact-meta-models.ts";
+import artifactMetasString from "../models/artifact-metas.json?raw";
+
+const artifactMetas: ArtifactMetas = JSON.parse(artifactMetasString) as ArtifactMetas;
+
+export default artifactMetas;
