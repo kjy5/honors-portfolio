5b2fdc7f01e22041fd3b24c551860b9d35d4d0f7
Kenneth Yang <kjy5@uw.edu>
2024-02-13 15:23:44 -0800

    🔧 fix: refactor LinkCard component to use LinkContent component for rendering link content

--- src/components/link-card.tsx
+++ src/components/link-card.tsx
@@ -1,6 +1,27 @@
 import { LinkMeta } from '../models/artifact-meta-models.ts';
 import { Avatar, Card, CardActionArea, CardContent, CardMedia, Link, Stack, Typography } from '@mui/material';
 
+function LinkContent(props: { title: string; description: string; faviconSrc: string; domain: string }) {
+  return (
+    <CardContent>
+      <Stack spacing={2}>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.title}
+        </Typography>
+
+        <Typography variant="body2" color="text.secondary">
+          {props.description}
+        </Typography>
+
+        <Stack direction={'row'} spacing={2} alignItems={'center'}>
+          {props.faviconSrc && <Avatar src={props.faviconSrc} />}
+          <Link href={`https://${props.domain}`}>{props.domain}</Link>
+        </Stack>
+      </Stack>
+    </CardContent>
+  );
+}
+
 function LinkCard({ linkMeta }: { linkMeta: LinkMeta }) {
   return (
     <Link href={linkMeta.url} underline={'none'}>
@@ -9,22 +30,12 @@ function LinkCard({ linkMeta }: { linkMeta: LinkMeta }) {
           {linkMeta.imageSrc && (
             <CardMedia component={'img'} height={180} image={linkMeta.imageSrc} alt={linkMeta.title} />
           )}
-          <CardContent>
-            <Stack spacing={2}>
-              <Typography gutterBottom variant="h5" component="div">
-                {linkMeta.title}
-              </Typography>
-
-              <Typography variant="body2" color="text.secondary">
-                {linkMeta.description}
-              </Typography>
-
-              <Stack direction={'row'} spacing={2} alignItems={'center'}>
-                {linkMeta.faviconSrc && <Avatar src={linkMeta.faviconSrc} />}
-                <Link href={`https://${linkMeta.domain}`}>{linkMeta.domain}</Link>
-              </Stack>
-            </Stack>
-          </CardContent>
+          <LinkContent
+            title={linkMeta.title}
+            description={linkMeta.description}
+            faviconSrc={linkMeta.faviconSrc}
+            domain={linkMeta.domain}
+          />
         </CardActionArea>
       </Card>
     </Link>
