dbb98402bc61baf9382383da72c95b2b92658e79
Kenneth Yang <kjy5@uw.edu>
2024-02-13 15:34:47 -0800

    🎨 style: improve formatting and comments in LinkContent component

--- src/components/link-card.tsx
+++ src/components/link-card.tsx
@@ -1,21 +1,29 @@
-import { LinkMeta } from '../models/artifact-meta-models.ts';
-import { Avatar, Card, CardActionArea, CardContent, CardMedia, Link, Stack, Typography } from '@mui/material';
+import {LinkMeta} from '../models/artifact-meta-models.ts';
+import {Avatar, Card, CardActionArea, CardContent, CardMedia, Link, Stack, Typography} from '@mui/material';
 
-function LinkContent(props: { title: string; description: string; faviconSrc: string; domain: string }) {
+/**
+ * Content of a link.
+ * @param title The title of the link.
+ * @param description The description of the link.
+ * @param faviconSrc The favicon of the link.
+ * @param domain The domain of the link.
+ * @constructor
+ */
+function LinkContent({title,  description, faviconSrc, domain}: { title: string; description: string; faviconSrc: string; domain: string }) {
   return (
     <CardContent>
       <Stack spacing={2}>
         <Typography gutterBottom variant="h5" component="div">
-          {props.title}
+          {title}
         </Typography>
 
         <Typography variant="body2" color="text.secondary">
-          {props.description}
+          {description}
         </Typography>
 
         <Stack direction={'row'} spacing={2} alignItems={'center'}>
-          {props.faviconSrc && <Avatar src={props.faviconSrc} />}
-          <Link href={`https://${props.domain}`}>{props.domain}</Link>
+          {faviconSrc && <Avatar src={faviconSrc} />}
+          <Link href={`https://${domain}`}>{domain}</Link>
         </Stack>
       </Stack>
     </CardContent>
@@ -23,8 +31,8 @@ function LinkContent(props: { title: string; description: string; faviconSrc: st
 }
 
 /**
- * Visual organization of a hyperlink.
- * @param linkMeta The metadata of the hyperlink.
+ * Rich representation of a link.
+ * @param linkMeta The metadata of the link.
  * @constructor
  */
 function LinkCard({ linkMeta }: { linkMeta: LinkMeta }) {
