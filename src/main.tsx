41518d0202a0e98de005363862785a79c5241513
Kenneth Yang <kjy5@uw.edu>
2024-02-20 17:06:22 -0800

    Switch to biome

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { darkTheme } from './themes.ts';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { darkTheme } from "./themes.ts";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 if (root) {
-  ReactDOM.createRoot(root).render(
-    <React.StrictMode>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </React.StrictMode>,
-  );
+	ReactDOM.createRoot(root).render(
+		<React.StrictMode>
+			<ThemeProvider theme={darkTheme}>
+				<CssBaseline />
+				<App />
+			</ThemeProvider>
+		</React.StrictMode>,
+	);
 }
--- src/utils/artifact-metas.ts
+++ src/utils/artifact-metas.ts
@@ -1,6 +1,8 @@
-import { ArtifactMetas } from '../models/artifact-meta-models.ts';
-import artifactMetasString from '../models/artifact-metas.json?raw';
+import { ArtifactMetas } from "../models/artifact-meta-models.ts";
+import artifactMetasString from "../models/artifact-metas.json?raw";
 
-const artifactMetas: ArtifactMetas = JSON.parse(artifactMetasString) as ArtifactMetas;
+const artifactMetas: ArtifactMetas = JSON.parse(
+	artifactMetasString,
+) as ArtifactMetas;
 
 export default artifactMetas;
