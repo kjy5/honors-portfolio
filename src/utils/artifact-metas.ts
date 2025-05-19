71cb15b9483fed22407d213956ee8af7b48819c1
Kenneth Yang <kjy5@uw.edu>
2025-05-18 22:12:00 -0700

    Reset history

--- src/utils/artifact-metas.ts
+++
@@ -1,8 +0,0 @@
-import type { ArtifactMetas } from "../models/artifact-meta-models.ts";
-import artifactMetasString from "../models/artifact-metas.json?raw";
-
-const artifactMetas: ArtifactMetas = JSON.parse(
-  artifactMetasString,
-) as ArtifactMetas;
-
-export default artifactMetas;
