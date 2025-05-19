d3e3939dbf57716100ce22e552c591c5729d570b
Kenneth Yang <82800265+kjy5@users.noreply.github.com>
2025-05-19 03:27:14 -0700

    Tailwind (#377)
    * Readme update
    
    * Add tailwind and daisy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://kjy5.github.io",
 	base: "honors-portfolio",
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
